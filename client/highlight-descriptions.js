f3b64a8188ed365936fdd21448d12453344f041d
Joel Chan <chozen86@gmail.com>
2017-10-27 17:42:08 -0400

    tweak highlight descriptions

--- client/highlight-descriptions.js
+++ client/highlight-descriptions.js
@@ -1,6 +1,6 @@
 highlightDescriptions = {
   "purpose": "What is the specific/immediate thing the paper's authors want to create or know?",
-  "mechanism": "If purpose is to create a 'system/method/model': How does the system/method/model work?",
+  "mechanism": "If purpose is to create a 'system/method/model': What is the proposed system/method/model, and how does it work?",
   "method": "If purpose is to produce 'empirical' findings: What research methods were used to answer the research question? If purpose is 'system/method/model': How did the authors evaluate their solution?",
   "finding": "If purpose is to produce 'empirical' findings: What did the authors find out? If purpose is 'system/method/model': Did the solution work?",
   "background": "What motivates the problem/question being addressed in this paper? What is the current state of the world for the problem/question? Why is this problem/question important (for society/other scientists)?",
